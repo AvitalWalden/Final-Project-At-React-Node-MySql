f2adf535cba3c165f207a1fc8ddba48119c1d78d
AvitalWalden <A0583295187@gmail.com>
2024-06-17 18:49:27 +0300

    mail

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -30,7 +30,6 @@ router.post("/", async (req, res) => {
         const { user, accessToken, refreshToken } = await logIn(username, password);
 
         res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
-
         const userLogIn = await getUser(user.user_id);
         delete userLogIn.address_id;
 
